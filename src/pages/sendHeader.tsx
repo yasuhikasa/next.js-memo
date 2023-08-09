7a234fb44fb0bd809db3610f976303742d2e3cc5
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-08-09 23:05:41 +0900

    first commit

---
+++ src/pages/sendHeader.tsx
@@ -0,0 +1,23 @@
+// pages/sendHeader.js
+import axios from 'axios';
+
+export default function SendHeader() {
+  const sendCustomHeader = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/post', {}, {
+        headers: {
+          'App-Type': 'settings',
+        },
+      });
+      console.log('Received response from B App:', response.data);
+    } catch (error) {
+      console.error('Error sending custom header:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={sendCustomHeader}>Send Custom Header</button>
+    </div>
+  );
+}
