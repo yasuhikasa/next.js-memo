eb07c1fcbf72f1694714337e67ab6e865188a7e2
yasuhikasa <yasuhikasa@yahoo.co.jp>
2023-09-26 22:34:06 +0900

    再度メモ追加

--- src/components/layoutMemo2.tsx
+++ src/components/layoutMemo2.tsx
@@ -37,3 +37,53 @@
 //     </div>
 //   </div>
 // )
+
+
+
+// 再度メモ
+// import React, { useEffect, useRef } from 'react';
+
+// // 各種定数を定義
+// const VIEW_WIDTH = 800;
+// const VIEW_HEIGHT = 480;
+
+// const Layout = () => {
+//   const containerRef = useRef(null);
+
+//   useEffect(() => {
+//     const ua = window.navigator.userAgent;
+//     const windowHeight = window.innerHeight;
+//     const windowWidth = window.innerWidth;
+//     const yScale = windowHeight / VIEW_HEIGHT;
+//     const xScale = windowWidth / VIEW_WIDTH;
+//     let minScale = Math.min(xScale, yScale);
+//     const container = containerRef.current;
+
+//     if (ua.match(/AISEG2|HomeCTRL/)) {
+//       // 解像度: 1024x600, 縦をフィットさせ、横は左寄せ
+//       const scale = windowHeight / 600;
+//       container.style.transform = `scale(${scale})`;
+//       container.style.marginRight = `${windowWidth - (1024 * scale)}px`;
+//     } else if (ua.match(/PEWDMSHomeViewer|DoorCTRL/)) {
+//       // 解像度: 800x480, 変更不要
+//       // Nothing to do
+//     } else if (ua.match(/android|ipad|iphone/)) {
+//       // 縦をフィットさせ、横は中央揃い
+//       container.style.transform = `scale(${yScale})`;
+//       container.style.marginLeft = `${(windowWidth - (VIEW_WIDTH * yScale)) / 2}px`;
+//     } else {
+//       // 画面フィットはせず中央揃いにする
+//       container.style.transform = `scale(${minScale})`;
+//       container.style.marginLeft = `${(windowWidth - (VIEW_WIDTH * minScale)) / 2}px`;
+//       container.style.marginTop = `${(windowHeight - (VIEW_HEIGHT * minScale)) / 2}px`;
+//     }
+//   }, []);
+
+//   return (
+//     <div ref={containerRef} style={{ width: VIEW_WIDTH, height: VIEW_HEIGHT }}>
+//       {/* コンテンツ */}
+//     </div>
+//   );
+// };
+
+// export default Layout;
